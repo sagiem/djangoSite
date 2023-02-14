f95cf3bb5e493eaf418fc4539b9e4bf17f6defab
Maksim Kashin <sagiems@gmail.com>
2023-02-14 14:13:51 +0600

    add css

--- women/urls.py
+++ women/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/<int:catid>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about),
+
 ]
--- women/views.py
+++ women/views.py
@@ -1,19 +1,30 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
+from .models import *
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 
 def index(request):
-    return HttpResponse("Страница приложения women")
+    post = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': post, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def categories(request, catid):
-    if(request.GET):
+    if (request.GET):
         print(request.GET)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
 
 def archive(request, year):
     if int(year) > 2023:
         return redirect('home', permanent=True)
     return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
