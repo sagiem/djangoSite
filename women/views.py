419cbbc3439157f732e757672930b3fe0307707e
Maksim Kashin <sagiems@gmail.com>
2023-02-15 13:13:47 +0600

    all

--- women/views.py
+++ women/views.py
@@ -1,30 +1,60 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 
 def index(request):
-    post = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': post, 'menu': menu, 'title': 'Главная страница'})
+    posts = Women.objects.all()
 
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def categories(request, catid):
-    if (request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
+def contact(request):
+    return HttpResponse("Обратная связь")
 
-def archive(request, year):
-    if int(year) > 2023:
-        return redirect('home', permanent=True)
-    return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
-    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=context)
+
+
